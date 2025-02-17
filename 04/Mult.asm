d9c3b914dff1dfbadaf95d1f33d15564839de793
Hiroki111 <9676341+Hiroki111@users.noreply.github.com>
2025-02-17 19:18:20 +0100

    project4 - Mult.asm

--- 04/Mult.asm
+++ 04/Mult.asm
@@ -1,31 +1,52 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/4/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// The algorithm is based on repetitive addition.
+
+//// Replace this comment with your code.
+
+// R2 = R0 * R1
+// Add R0 to R2, do it R1 times
+
+// set i, n, sum
+// @i=16 
+@i
+M=1
+@R1
+D=M
+// @n=17
+@n
+M=D
+// initialize R2
 @R2
 M=0
 
-@R0
+(LOOP)
+// if i > n, stop
+@i
 D=M
-@LOOP
+@n
+// i - n 
+D=D-M
+@STOP
 D;JGT
 
-@END
+// M     = D     + M
+// R[R2] = R[R0] + R[R2]
+@R0
+D=M
+@R2
+M=D+M
+
+@i
+M=M+1
+@LOOP
 0;JMP
 
-(LOOP)
-@R2
-D=M
-
-@R1
-D=D+M
-
-@R2
-M=D
-
-@R0
-D=M-1
-M=D
-
-@LOOP
-D;JGT
-
-(END)
-@END
+(STOP)
+@STOP
 0;JMP
